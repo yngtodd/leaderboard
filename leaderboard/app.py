725b6f9b7bd3ebb1b37005ec77fd4ee77f449554
yngtodd <young.todd.mk@gmail.com>
2021-02-28 13:41:20 -0500

    Add data loading

--- leaderboard/app.py
+++ leaderboard/app.py
@@ -1,10 +1,14 @@
-from config import banner
 from login import user_login
+from config import (
+    banner, load_competition_data
+)
 
 
 def main():
     banner()
     user_login()
+    load_competition_data()
+
 
 
 if __name__=="__main__":
--- leaderboard/config.py
+++ leaderboard/config.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import streamlit as st
 
@@ -9,4 +10,14 @@ def banner():
 
 def load_competition_data():
     files = os.listdir("competition")
+
+    if "competition_data.csv" not in files or "config.json" not in files:
+        st.sidebar.text("Admin please add competition data.")
+    else:
+        test_data = pd.read_csv("competition/data.csv")
+        with open("competition/config.json") as f:
+            config = json.load(f)
+
+        return data, config
+
 
